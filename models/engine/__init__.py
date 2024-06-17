993a29598e8000a0a959d6c50d730d17d1a0b364
JerryKebede <eyerusalem_ke@yahoo.com>
2024-06-17 06:29:09 -0700

    edit

--- models/engine/__init__.py
+++ models/engine/__init__.py
@@ -1,3 +1,1 @@
-#!/usr/bin/python3
-""" shebang line - defines where the interpreter is located """
-""" init file in models/engine """
+
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,49 +1,77 @@
 #!/usr/bin/python3
-""" shebang line - defines where the interpreter is located """
+"""
+Module for serializing and deserializing data
+"""
 import json
-import os.path
-from ..base_model import BaseModel
-from ..user import User
-from ..state import State
-from ..place import Place
-from ..amenity import Amenity
-from ..city import City
-from ..review import Review
-""" import moduls """
+import os
+from models.base_model import BaseModel
+from models.user import User
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.city import City
 
 
 class FileStorage:
-    """ serializes instances to a JSON file
-    and deserializes JSON file to instances """
+    """
+    FileStorage class for storing, serializing and deserializing data
+    """
     __file_path = "file.json"
-    __objects = {}
-    """ Private class attributes """
 
-    def all(self):
-        """ Public instance method that returns the dictionary __objects """
-        return FileStorage.__objects
+    __objects = {}
 
     def new(self, obj):
-        """ Public instance method that sets in __objects
-        the obj with key <obj class name>.id """
-        key = "{}.{}".format(obj.__class__.__name__, obj.id)
+        """
+         Sets an object in the __objects dictionary with a key of 
+         <obj class name>.id.
+        """
+        obj_cls_name = obj.__class__.__name__
+
+        key = "{}.{}".format(obj_cls_name, obj.id)
+
         FileStorage.__objects[key] = obj
 
+
+    def all(self):
+        """
+        Returns the __objects dictionary. 
+        It provides access to all the stored objects.
+        """
+        return  FileStorage.__objects
+
+
     def save(self):
-        """ Public instance method that serializes
-        __objects to the JSON file """
-        dict_temp = {}
-        for key, value in FileStorage.__objects.items():
-            dict_temp[key] = value.to_dict()
-            with open(FileStorage.__file_path, "w") as f:
-                f.write(json.dumps(dict_temp))
+        """
+        Serializes the __objects dictionary into 
+        JSON format and saves it to the file specified by __file_path.
+        """
+        all_objs = FileStorage.__objects
+
+        obj_dict = {}
+
+        for obj in all_objs.keys():
+            obj_dict[obj] = all_objs[obj].to_dict()
+
+        with open(FileStorage.__file_path, "w", encoding="utf-8") as file:
+            json.dump(obj_dict, file)
 
     def reload(self):
-        """ Public instance method that deserializes
-        the JSON file to __objects """
+        """
+        This method deserializes the JSON file
+        """
         if os.path.isfile(FileStorage.__file_path):
-            with open(FileStorage.__file_path, "r") as f:
-                dict_temp = json.loads(f.read())
-                for key, value in dict_temp.items():
-                    obj = eval(value['__class__'])(**value)
-                    FileStorage.__objects[key] = obj
+            with open(FileStorage.__file_path, "r", encoding="utf-8") as file:
+                try:
+                    obj_dict = json.load(file)
+
+                    for key, value in obj_dict.items():
+                        class_name, obj_id = key.split('.')
+
+                        cls = eval(class_name)
+
+                        instance = cls(**value)
+
+                        FileStorage.__objects[key] = instance
+                except Exception:
+                    pass
